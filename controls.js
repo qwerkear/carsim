61cc007870a678331a3511fe678983dd0d02d286
Jamie <qwer.kear@gmail.com>
2024-03-18 20:39:27 -0400

    add dummy car, add off road detection

--- controls.js
+++ controls.js
@@ -1,11 +1,18 @@
 class Controls {
-    constructor() {
+    constructor(type) {
         this.forward = false
         this.left = false
         this.right = false
         this.reverse = false
 
-        this.#addKeyboardListeners()
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners()
+                break
+            case "DUMMY":
+                this.forward = true
+                break
+            }
     }
 
     //TO accept user input, we map the keyup and keydown values of the arrow keys
